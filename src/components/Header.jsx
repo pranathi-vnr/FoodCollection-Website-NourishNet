85c08944b6b32169712ce467aa4d63301b004280
kancharlasaisruthi <saikancharla08@gmail.com>
2024-10-25 16:10:39 +0530

    edited home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,21 @@ import logo1 from '../assets/logo1.jpg'
 import './H1.css'
 function Header() {
   return (
-    <div className='bg-light'>
-    <header className='header'>
+    <div className='heading'>
+    <header className='header '>
       <div className='logo-container'>
         <img src={logo1} alt="logo" width={75} className='mx-3 rounded-circle'/>
       </div>
       <nav className='nav'>
         <ul className='nav-list' >
           <li className="nav-item ml-2">
-            <Link to="" className='nav-link text-black'>Home</Link>
+            <Link to="" className='nav-link ' style={{  color: 'rgb(60, 87, 12)'}}>Home</Link>
           </li>
           <li className="nav-item ml-2">
-            <Link to="register" className='nav-link text-black'>Register</Link>
+            <Link to="register" className='nav-link ' style={{color: 'rgb(60, 87, 12)'}}>Register</Link>
           </li>
           <li className="nav-item ml-2">
-            <Link to="login" className='nav-link text-black'>Login</Link>
+            <Link to="login" className='nav-link btn ' style={{color: 'white', background:'rgb(107, 142, 35)'}}>Login</Link>
           </li>
         </ul>
       </nav>
