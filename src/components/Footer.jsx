d5443215e0e9524745a09ea9f091b2698fe9f7d2
Saranyavellanki <siriiv7@gmail.com>
2024-10-26 12:47:00 +0530

    commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+
 function Footer() {
   return (
-    <div className='footer  d-flex justify-content-around p-5'>
-     <div>
-    <p className='lead'> Contact:xxxxxx8906</p>
-    <p className='lead'>Office:street no:xxx,city:xxxxx,state:xxxxxx</p>
-     </div>
-     <div>
-        <p className='lead'>email:nourishnet@gmail.com</p>
-        <p className='lead'> Thank you</p>
-     </div>
+    <div>
+      <footer>
+        <div className="footer-container">
+          <div className="footer-section donations">
+            <h3>DONATIONS</h3>
+            <p>Currently My Angels academy supports 300+ underprivileged children with items like sports equipments, books, stationery, nutrition, clothes, jerseys, conveyance, medical and other day-to-day needs. Every month substantial amount is required to carry out these activities.</p>
+            <p>Click the below button if you wish to Donate.</p>
+            <button className="donate-button">DONATE</button>
+          </div>
+
+          <div className="footer-section newsletter">
+            <h3>SUBSCRIBE TO OUR NEWSLETTER</h3>
+            <input type="email" placeholder="Email Address*" />
+            <button className="subscribe-button">SUBSCRIBE</button>
+          </div>
+
+          <div className="footer-section contact">
+            <h3>CONTACT US</h3>
+            <address>
+              WZ-49/2A F/F, Budella Village,<br />
+              Near Community Hall, Vikas Puri,<br />
+              New Delhi – 110018 Delhi, India<br /><br />
+              Email : myangelsacademy@gmail.com<br />
+              Landline: 011 – 47039983,<br />
+              Mobile: +91 7701927625, +91 9654793872<br /><br />
+              Registered Office:<br />
+              31, Naval apartment,<br />
+              Vasundhara Enclave Delhi 110096
+            </address>
+            <div className="social-icons">
+              <a href="#"><i className="fab fa-facebook-f"></i></a>
+              <a href="#"><i className="fab fa-instagram"></i></a>
+              <a href="#"><i className="fab fa-linkedin-in"></i></a>
+              <a href="#"><i className="fab fa-youtube"></i></a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
